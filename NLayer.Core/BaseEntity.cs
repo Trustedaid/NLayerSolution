da8e14ea648b9c010d6953a1760178a3279a31bf
Eren OĞUZ <erenoguz@unilogistix.com>
2023-08-08 17:55:31 +0300

    Initial Comit

---
+++ NLayer.Core/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace NLayer.Core;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime UpdateDate { get; set; }
+}
